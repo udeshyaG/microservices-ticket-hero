1c04fea839eef50e3252c81dcbb18cd7a50af228
Udeshya <udeshyagiri@gmail.com>
2021-05-18 12:04:37 +0530

    added breaking change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,8 @@ it('Reponds with details about the current user', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
+  // .expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
