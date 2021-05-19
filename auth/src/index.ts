53d61c0cb70c321b53ff96d0860a0e1ba62b43bc
Udeshya <udeshyagiri@gmail.com>
2021-05-19 07:12:30 +0530

    small change to auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting up');
+  console.log('Starting up... Checking for docker build');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined as an Environment variable');
